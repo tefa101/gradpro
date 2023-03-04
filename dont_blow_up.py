a8ad16a016335aff68e8801e9ac12fc347e0db7e
tefa101 <115157825+tefa101@users.noreply.github.com>
2023-03-04 18:51:08 +0200

    depresseed wrbna

--- dont_blow_up.py
+++ dont_blow_up.py
@@ -25,6 +25,7 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
 
 data = pd.read_csv('scaled_data.csv')
+allfigs = PdfPages('model_plots.pdf')
 print(data.head())
 print(data['y'][:10])
 print(data.columns)
@@ -36,5 +37,34 @@ model = ExtraTreesClassifier()
 model.fit(X,y)
 
 feature_importance = pd.Series(model.feature_importances_, index = X.columns)
-feature_importance.nlargest(10).plot(kind = 'barh')
+ax = feature_importance.nlargest(10).plot(kind = 'barh')
 plt.show()
+allfigs.savefig(ax)
+
+#Train and Test Split (80:20)
+
+X=data.drop(['pdays','month','con.price.idx','loan','housing','emp.var.rate','y'],axis=1)
+y=data.y
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=0.8,random_state=1)
+print("Input Training:",X_train.shape)
+print("Input Test:",X_test.shape)
+print("Output Training:",y_train.shape)
+print("Output Test:",y_test.shape)
+
+#------------- MODEL SELECTION---------------s-#
+
+#creating the objects
+logreg_cv = LogisticRegression(random_state=0)
+dt_cv=DecisionTreeClassifier()
+knn_cv=KNeighborsClassifier()
+svc_cv=SVC()
+nb_cv=BernoulliNB()
+cv_dict = {0: 'Logistic Regression', 1: 'Decision Tree',2:'KNN',3:'SVC',4:'Naive Bayes'}
+cv_models=[logreg_cv,dt_cv,knn_cv,svc_cv,nb_cv]
+
+
+for i,model in enumerate(cv_models):
+    print("{} Test Accuracy: {}".format(cv_dict[i],cross_val_score(model, X, y, cv=10, scoring ='accuracy').mean()))
+
+
