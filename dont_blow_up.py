e4ee7a58210531fff121ffcc520326072d9f0a91
tefa101 <115157825+tefa101@users.noreply.github.com>
2023-03-04 19:28:37 +0200

    kill me

--- dont_blow_up.py
+++ dont_blow_up.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 from matplotlib.backends.backend_pdf import PdfPages
@@ -18,6 +19,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 #Metrics Phase
 from sklearn import metrics
@@ -39,11 +41,11 @@ model.fit(X,y)
 feature_importance = pd.Series(model.feature_importances_, index = X.columns)
 ax = feature_importance.nlargest(10).plot(kind = 'barh')
 plt.show()
-allfigs.savefig(ax)
+
 
 #Train and Test Split (80:20)
 
-X=data.drop(['pdays','month','con.price.idx','loan','housing','emp.var.rate','y'],axis=1)
+X=data.drop(['pdays','month','cons.price.idx','loan','housing','emp.var.rate','y'],axis=1)
 y=data.y
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=0.8,random_state=1)
@@ -64,7 +66,31 @@ cv_dict = {0: 'Logistic Regression', 1: 'Decision Tree',2:'KNN',3:'SVC',4:'Naive
 cv_models=[logreg_cv,dt_cv,knn_cv,svc_cv,nb_cv]
 
 
-for i,model in enumerate(cv_models):
-    print("{} Test Accuracy: {}".format(cv_dict[i],cross_val_score(model, X, y, cv=10, scoring ='accuracy').mean()))
+# for i,model in enumerate(cv_models):
+#     print("{} Test Accuracy: {}".format(cv_dict[i],cross_val_score(model, X, y, cv=10, scoring ='accuracy').mean()))
 
 
+# # logistic regression has the highst score 
+# so we will use it for the rest of the model selection
+
+#------------------ LOGISTIC REGRESSION -----------------#
+# FOOOCUUUSSSS *** hard **** important
+
+# Logistic regression with Hyperparameter tuning
+
+param_grid = {'C': np.logspace(-4, 4, 50), 'penalty':['l1', 'l2']}
+clf = GridSearchCV(LogisticRegression(random_state=0), param_grid,cv=5, verbose=0,n_jobs=-1)
+best_model = clf.fit(X_train,y_train)
+print(best_model.best_estimator_)
+print("The mean accuracy of the model is:",best_model.score(X_test,y_test))
+
+# we got 94% accuracy with logistic regression 
+logreg = LogisticRegression(C=0.18420699693267145, random_state=0)
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+# the Confusion Matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n",confusion_matrix)
+print("Classification Report:\n",classification_report(y_test, y_pred))
